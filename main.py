e0db74dee9476485e6d78029194115c0f28a4701
Daniel Grigoriev <danielgrigoriev7@gmail.com>
2025-03-12 23:00:55 +0200

    Implemented scraping structure with nodriver

--- main.py
+++ main.py
@@ -9,275 +9,134 @@ import os
 import sqlite3
 from datetime import datetime
 from googletrans import Translator
+import nodriver as nd
+import json
+import time
+import asyncio
+from dataclasses import dataclass
 load_dotenv()
 
-hebrew_to_english_brand_map = {
-    "ימאהה": "Yamaha",
-    "סי אף מוטו": "CFMoto",
-    "הונדה": "Honda",
-    "ק.ט.מ": "KTM",
-    "קליבלנד": "Cleveland",
-    "סאן יאנג": "SYM",
-    "קאוואסאקי": "Kawasaki",
-    "רויאל": "Royal Enfield",
-    "דוקאטי": "Ducati",
-    "טריומף": "Triumph",
-    "ב.מ.וו": "B.M.W",
-    "מוטוגוצי": "Moto Gucci",
-    "בנלי": "Benelli",
-    "הוסקוורנה": "Husqvarna",
-    "סוזוקי": "Suzuki",
-    "הרלי": "Harley Davidson",
-    "אינדיאן": "Indian",
-    "Voge": "Voge",
-    "פאנטיק": "Fantic"
-}
 
-hebrew_to_english_color_map = {
-    "לבן": "White",
-    "לבן מטאלי": "White",
-    "שחור": "Black",
-    "אדום": "Red",
-    "כחול בהיר": "Light Blue",
-    "צהוב": "Yellow",
-    "כחול": "Blue",
-    "אפור": "Gray",
-    "סגול": "Purple",
-    "כתום": "Orange",
-    "ירוק": "Green",
-    "חום מטאלי": "Metallic Brown",
-    "כסוף מטאלי": "Silver",
-
-}
-
-
+@dataclass
 class MotorcycleListing:
-    def __init__(self, listing_id, creation_date, model_and_brand, model_year, engine_displacement, license_rank, kilometrage, amount_of_owners, color, listed_price):
+    listing_id: int
+    creation_date: str
+    location_of_seller: str
+    brand: str
+    model_name: str
+    model_year: str
+    engine_displacement: int
+    license_rank: str
+    kilometrage: int
+    amount_of_owners: int
+    color: str
+    listed_price: int
+    active: bool = True
 
-        self.listing_id = listing_id
-        self.creation_date = format_date(creation_date)
-        self.brand, self.model_name = exctract_brand_and_model(model_and_brand)
-        self.model_year = model_year
-        engine_displacement = int(engine_displacement.replace(",", ""))
-        self.engine_displacement = engine_displacement
-
-        # Inits license rank
-        if license_rank is None:
-            if engine_displacement < 500:
-                self.license_rank = 'A1'
-            elif engine_displacement > 500:
-                self.license_rank = 'A'
-            else:
-                self.license_rank = 'N/A'
+    def __post_init__(self):
+        if self.license_rank is None:
+            self.license_rank = 'A' if self.engine_displacement > 500 else 'A1'
         else:
-            if "47" in license_rank:
-                self.license_rank = "A1"
-            else:
-                self.license_rank = "A"
-        # Kilometrage
-        self.kilometrage = int(re.sub(r"[^\d]", "", kilometrage))
-        # Amount of owners
-        self.amount_of_owners = amount_of_owners
-
-        # Initilizes Color translates it from Heb to Eng
-        if len(color.split()) > 1:
-            color = color.split()[0]
-        if color is None:
-            print(color)
-            color = "Other"
-        self.color = hebrew_to_english_color_map.get(color)
-
-        # Inits the listed price into an int
-        if listed_price == "לא צוין מחיר":
-            self.listed_price = None
-        else:
-            self.listed_price = int(re.sub(r"[^\d]", "", listed_price))
-
-        self.active = True
-
-    def __str__(self):
-        return (
-            f"LISTING ID:{self.listing_id}\n"
-            f"CREATION DATE:{self.creation_date}\n"
-            f"BRAND:{self.brand}\n"
-            f"MODEL:{self.model_name}\n"
-            f"MODEL_YEAR:{self.model_year}\n"
-            f"ENGINE CC:{self.engine_displacement}\n"
-            f"LICENSE RANK:{self.license_rank}\n"
-            f"YEAR:{self.model_year}\n"
-            f"KILOMETRAGE:{self.kilometrage}\n"
-            f"YAD:{self.amount_of_owners}\n"
-            f"COLOR:{self.color}\n"
-            f"LISTED PRICE:{self.listed_price}\n"
-            f"ACTIVE:{self.active}\n"
-        )
+            self.license_rank = exctract_license_rank(self.license_rank)
 
 
-# Formats DD/MM/YYYY to the ISO YYYY-MM-DD format for sql
-def format_date(date: str) -> str:
-    date_elements = date[1:].split("/")
-    formatted_date = f"20{date_elements[-1]}-{date_elements[1]}-{date_elements[0]}"
-    return formatted_date
+@dataclass
+class ExctracedPage:
+    page_num: int
+    max_page_available: int
+    listings: list[MotorcycleListing]
 
 
-# Returns a list of links for each individual listing in the page.
-def exctract_individual_listings_links(main_page_html: BeautifulSoup, base_url: str) -> list[str]:
-    listings = main_page_html.find_all('div', class_=re.compile("feed-item-base_feedItemBox"))
-    links = []
-    for listing in listings:
-        a_tag = listing.find('a', href=True)
-        if a_tag:
-            full_url = urljoin(base_url, a_tag['href'])
-            links.append(full_url)
-    return links
-
-
-# Recieves string with the brand and model of a motorycle and exctracts model and brand
-def exctract_brand_and_model(s: str) -> tuple[str]:
-    brand = None
-    model = ""
-    if "KTM" in s:
-        brand = "KTM"
-        model = s.replace("KTM", "").strip()
+# Exctracts the license rank from string
+def exctract_license_rank(s: str) -> str:
+    if "47" in s:
+        return "A1"
     else:
-        model = ""
-        words = s.split()
-        for word in words:
-            clean_word = word.strip()
-            if clean_word in hebrew_to_english_brand_map:
-                brand = hebrew_to_english_brand_map[clean_word]
-            else:
-                model += f" {clean_word}"
-
-        model = model.replace("אנפילד", "")
-        model = model.replace("הרלי", "")
-        model = model.strip()
-
-    if brand is None:
-        brand = "Other"
-
-    return brand, model
+        return "A"
 
 
-# Recieves html of a listing page of a motorcycle and returns the yad2 mototrycle listing class
-def scrape_listing(listing_html: BeautifulSoup) -> MotorcycleListing:
-
-    # Exctracts listing id
-    listing_id = listing_html.find_all('div', class_=re.compile("ad_adNumber"))[0].get_text(strip=True)
-
-    # Exctracts creation date of the listing
-    creation_date = listing_html.find('span', class_=re.compile("report-ad_createdAt")).get_text(strip=True).split()[-1]
-
-    # Exctracts kilometrage,color,license_rank and engine displacement
-    dl = listing_html.find("dl")
-    children = dl.find_all(["dd", "dt"])
-    details = {}
-    for i in range(0, len(children), 2):
-        label = children[i].get_text(strip=True)
-        value = children[i+1].get_text(strip=True)
-        details[label] = value
-    kilometrage = details.get("קילומטראז׳")
-    color = details.get("צבע")
-    license_rank = details.get("דרגת רשיון")
-    engine_displacement = details.get("נפח מנוע")
-
-    # Exctracts the amount of owners and year
-    moto_data = listing_html.find_all('span', class_=re.compile("details-item_itemValue"))
-    model_year = moto_data[0].get_text(strip=True)
-    amount_of_owners = moto_data[1].get_text(strip=True)
-
-    # Exctracts listed price
-    listed_price = listing_html.find("span", {"data-testid": "price"}).get_text(strip=True)
-
-    # Exctracts model and brand
-    model_and_brand = listing_html.find("h1", {"data-nagish": "upper-heading-title"}).get_text(strip=True)
-
-    motorcycle_listing = MotorcycleListing(listing_id, creation_date, model_and_brand, model_year, engine_displacement, license_rank, kilometrage, amount_of_owners, color, listed_price)
-    return motorcycle_listing
+# Exctracts english variant name from listing dict
+def exctract_english_variant(listing: dict) -> str:
+    s = listing.get('textEng')
+    if s is None:
+        s = listing['text']
+    return s
 
 
-proxy_url = "https://api.brightdata.com/request"
-headers = {
-    "Authorization": f"Bearer {os.getenv("API_TOKEN")}",
-    "Content-Type": "application/json"
-}
-payload = {
-    "zone": os.getenv("ZONE"),
-    "format": "raw",
-    "method": "GET"
-}
+# Exctracts all yad2 page data
+async def exctract_page_data(page_num: int, browser: nd.Browser) -> ExctracedPage:
+
+    url = f"http://www.yad2.co.il/vehicles/motorcycles?page={page_num}"
+    print(f"started scraping page{page_num}")
+    # Goes to the given page in Yad2
+    tab = await browser.get(url)
+    await tab.sleep(5)
+
+    script_element = await tab.query_selector("#__NEXT_DATA__")
+    await tab.sleep(3)
+
+    json_text = await script_element.get_html()
+    await tab
+    print(f"succesfully parsed json data from {page_num}")
+
+    # Parses and loads needed data into one list with all listings
+    soup = BeautifulSoup(json_text, 'html.parser')
+    json_text = soup.find('script', id='__NEXT_DATA__', type='application/json').get_text(strip=True)
+    data = json.loads(json_text)
+    commercial_listings = data["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"]["data"]["commercial"]
+    private_listings = data["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"]["data"]["private"]
+    all_listings = commercial_listings + private_listings
+
+    # For each listing creates a MotorcycleListing dataclass
+    all_moto_listings = []
+    for listing in all_listings:
+        # Debugging printouts
+        print("listing_id:", listing['adNumber'])
+        print("creation_date:", datetime.fromisoformat(listing['dates']['createdAt']).year)
+        print("location_of_seller:", listing['address']['area']['textEng'])
+        print("brand:", exctract_english_variant(listing['manufacturer']))
+        print("model_name:", exctract_english_variant(listing['model']))
+        print("model_year:", listing['vehicleDates']['yearOfProduction'])
+        print("engine_displacement:", listing['engineVolume'])
+        print("license_rank:", listing['license'].get('text'))
+        print("kilometrage:", listing['km'])
+        print("amount_of_owners:", listing['hand']['id'])
+        print("color:", listing['color']['textEng'])
+        print("listed_price:", listing['price'])
+
+        moto_listing = MotorcycleListing(
+            listing_id=listing['adNumber'],
+            creation_date=datetime.fromisoformat(listing['dates']['createdAt']).year,
+            location_of_seller=listing['address']['area']['textEng'],
+            brand=exctract_english_variant(listing['manufacturer']),
+            model_name=exctract_english_variant(listing['model']),
+            model_year=listing['vehicleDates']['yearOfProduction'],
+            engine_displacement=listing['engineVolume'],
+            license_rank=listing['license'].get('text'),
+            kilometrage=listing['km'],
+            amount_of_owners=listing['hand']['id'],
+            color=listing['color']['textEng'],
+            listed_price=listing['price']
+        )
+        print(moto_listing)
+        all_moto_listings.append(moto_listing)
+
+    max_page = data["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"]["data"]["pagination"]["pages"]
+    exctracted_page = ExctracedPage(page_num=page_num, max_page_available=max_page, listings=all_moto_listings)
+    return exctracted_page
 
 
-def main():
-    connection = sqlite3.connect("yad2_motorcycles_listings.db")
-    cursor = connection.cursor()
-
-    page = 4
+async def main():
+    page = 1
+    browser = await nd.start()
     while True:
-        print(f"Started Scraping Page:{page}")
-        url = f"http://www.yad2.co.il/vehicles/motorcycles?page={page}"
-        payload["url"] = url
-        response = requests.request("POST", proxy_url, json=payload, headers=headers)
-
-        if response.status_code != 200:
-            print(f"Failed to retrieve page number {page}")
+        exctracted_page = await exctract_page_data(page, browser)
+        if exctracted_page.max_page_available == exctracted_page.page_num:
+            print("Scraped all pages")
             break
-
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        # Gathers links for each individual listing page in the original page
-        links = exctract_individual_listings_links(soup, url)
-
-        # Loops over every listing in the current page
-        for link in links:
-            print(f"Started Scraping listing:{link}\n")
-            # Retrieves page HTML
-            payload["url"] = link
-            response = requests.request("POST", proxy_url, json=payload, headers=headers)
-            soup = BeautifulSoup(response.text, "html.parser")
-            moto_listing = scrape_listing(soup)
-            print(moto_listing)
-            query = """
-                INSERT INTO motorcycle_listings (
-                listing_id,
-                creation_date,
-                brand,
-                model_name,
-                model_year,
-                engine_displacement,
-                license_rank,
-                kilometrage,
-                amount_of_owners,
-                color,
-                listed_price,
-                active,
-                last_seen
-            )
-            VALUES (
-                :listing_id,
-                :creation_date,
-                :brand,
-                :model_name,
-                :model_year,
-                :engine_displacement,
-                :license_rank,
-                :kilometrage,
-                :amount_of_owners,
-                :color,
-                :listed_price,
-                :active,
-                date('now')
-            )
-            ON CONFLICT(listing_id) DO UPDATE
-            SET last_seen = date('now')
-            """
-            cursor.execute(query, vars(moto_listing))
-            connection.commit()
+        print(f"Succesfully scraped page number {page}")
         page += 1
-
-    cursor.close()
-    connection.close()
 
 
 if __name__ == "__main__":
-    main()
+    nd.loop().run_until_complete(main())
